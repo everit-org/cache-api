d3e204558413ffe39b4d5ad4245505fb8ba71946
balazs-zsoldos <balazs.zsoldos@everit.biz>
2014-02-24 17:03:43 +0100

    Changing license headers and pom

--- src/main/java/org/everit/osgi/cache/api/CacheFactory.java
+++ src/main/java/org/everit/osgi/cache/api/CacheFactory.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Everit - Cache API.
+ *
+ * Everit - Cache API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - Cache API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Cache API.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.osgi.cache.api;
 
 import java.util.Map;
@@ -18,6 +34,6 @@ public interface CacheFactory {
      *            Additional configurations
      * @return
      */
-    Cache<String, Object> createCache(int maxEntries, Map<String, Object> params);
+    Cache<String, Object> createCache(int maxEntries, Map<String, ?> params);
 
 }
